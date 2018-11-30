2a75c965901ffcb8dfb7c595c62e77464fb5dbcc
Brady Bentley <bradybentley@Bradys-MacBook-Pro.local>
2018-11-30 15:13:24 -0700

    Step up stack view and IBOutlets

--- gitPract/ViewController.swift
+++ gitPract/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
 
 }
 
